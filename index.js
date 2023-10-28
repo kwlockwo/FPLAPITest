00d4fa403333e07f256f9bd13c5a7640aab2674c
Mayura Vivekananda <km.vivekananda@gmail.com>
2023-10-27 22:03:37 -0400

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3001;
+
+app.get('/api', async (req, res) => {
+    try {
+        const bootstrapResponse = await axios.get('https://fantasy.premierleague.com/api/bootstrap-static/', {
+            headers: {
+                'User-Agent': 'FPL-Website/1.0'
+            }
+        });
+        try {
+            const currentGameweek = bootstrapResponse.data.events.find(event => event.is_current).id;
+        res.json({ currentGameweek });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to fetch current gameweek' });
+        }
+    } catch (error) {
+        console.log(error.response.data);
+        console.error(error);
+        res.status(500).json({ error: 'Failed to connect to FPL API' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
